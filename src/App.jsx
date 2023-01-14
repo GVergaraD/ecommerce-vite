5aa7bc607031677819d91f27f9e2a4861d7b5e1a
GVergaraD <gvergara182@gmail.com>
2023-01-14 00:46:55 -0300

    commit base

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Home from './pages/Home'
+import ProductList from './pages/ProductList'
+import Product from './pages/Product'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Cart from './pages/Cart'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Home/>
+      <Cart />
     </div>
   )
 }
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import ArrowLeftOutlinedIcon from '@mui/icons-material/ArrowLeftOutlined';
 import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
+import { sliderItems } from '../data';
+import { mobile } from '../responsive';
+
 
 const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
-    background-color: coral;
+    position: relative;
+    overflow: hidden;
+    /* background-color: coral; */
+    ${mobile({ display: "none"})}
 `
 
 const Arrow = styled.div`
@@ -30,6 +36,9 @@ const Arrow = styled.div`
 
 const Wrapper = styled.div`
     height:100%;
+    display: flex;
+    transition: all 1.5s ease;
+    transform: translateX(${(props) => props.slideIndex * -100}vw);
 `
 
 const Slide = styled.div`
@@ -37,37 +46,72 @@ const Slide = styled.div`
     height: 100vh;
     display: flex;
     align-items: center;
+    background-color: #${(props)=>props.bg};
 `
 
 const ImgContainer = styled.div`
+    height: 100%;
     flex: 1;
 `
 
 const Image = styled.img`
-
+    height: 80%;
 `
 
 const InfoContainer = styled.div`
     flex: 1;
+    padding: 50%;
+`
+
+const Title = styled.h1`
+    font-size: 70px;
+`
+
+const Desc = styled.p`
+    margin: 50px 0px;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+`
+
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
 `
 
 const Slider = () => {
+    const [slideIndex, setSlideIndex] = useState(0)
+    const handleClick = (direction) => {
+    
+        if(direction==="left"){
+            setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2)
+        } else {
+            setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0)
+        }
+    }
+
   return (
     <Container>
-        <Arrow direction="left">
+        <Arrow direction="left" onClick={() => handleClick("left")}>
             <ArrowLeftOutlinedIcon/>
         </Arrow>
-        <Wrapper>
-            <Slide>
+        <Wrapper slideIndex={slideIndex}>
+            {sliderItems.map((item) => (
+            <Slide bg={item.bg} key={item.id}>
                 <ImgContainer>
-                    <Image src="https://i.ibb.co/XsdmR2c/1.png" />
+                    <Image src={item.img} />
                 </ImgContainer>
                 <InfoContainer>
-
+                    <Title>{item.title}</Title>
+                    <Desc>{item.desc}</Desc>
+                    <Button>SHOW NOW</Button>
                 </InfoContainer>
             </Slide>
+                ))}
         </Wrapper>
-        <Arrow direction="right">
+        <Arrow direction="right" onClick={() => handleClick("right")}>
             <ArrowRightOutlinedIcon/>
         </Arrow>
     </Container>
